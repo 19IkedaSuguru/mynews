54f989ff55e85673e84d5d585b991c181dcfaee9
Ikeda Suguru <kareidaisuki000@gmail.com>
2019-07-28 01:28:36 +0000

    “課題17と18”

--- app/Http/Controllers/Admin/NewController.php
+++ app/Http/Controllers/Admin/NewController.php
@@ -15,39 +15,3 @@ class NewController extends Controller
 
 }
 
-namespace App\Http\Controllers\Admin;
-
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-
-class profileController extends Controller
-{
-    //応用】 Admin/ProfileControllerに、
-    //以下のadd, create, edit, update 
-    //それぞれのActionを追加してみましょう。
-     public function add()
-  {
-      return view('admin.profile.create');
-  }
-    
-}
-namespace App\Http\Controllers\Admin;
-
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-
-class profileController extends Controller
-{
-    public function create()
-  {
-      return redirect('admin/profile/create');
-  }
-  public function edit()
- {
- return view('admin.profile.edit');
- }
- public function update()
- {
- return redirect('admin/profile/edit');
- }
-}
